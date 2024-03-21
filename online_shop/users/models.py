c816ae5c30fdb1cb920c4d3ca0bc741db0aa5b7e
Rene-W <rew01@web.de>
2024-03-21 15:53:58 +0100

    changed models and views

--- online_shop/users/models.py
+++ online_shop/users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    userid = models.CharField(max_length=50, unique=True)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    age = models.PositiveIntegerField()
+    order_history = models.ManyToManyField('Order', blank=True)
+    shopping_cart = models.ManyToManyField('Product', blank=True)
+    wishlist = models.ManyToManyField('Product', blank=True)
+
