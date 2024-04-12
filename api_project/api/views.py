d8de88b65cf3c59eeb5b6dac45434f9241d91f3c
nataliedutz <natalie.dutz@dci-student.org>
2024-04-12 15:10:28 +0200

    Add README, add Swagger and redoc documentation for APIs

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,36 +1,35 @@
-# api/views.py
-from rest_framework import generics, permissions
+
+from rest_framework import generics
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.reverse import reverse
+from rest_framework.views import APIView
 from .models import Contact, Genre
 from .serializers import ContactSerializer, GenreSerializer
-
+from .permissions import IsAdminOrReadOnly
 
 class APIRoot(APIView):
     def get(self, request, format=None):
         return Response({
             'contacts': reverse('contact-list', request=request, format=format),
             'genres': reverse('genre-list', request=request, format=format),
-            # Add other API endpoints here
         })
 
 class ContactListCreate(generics.ListCreateAPIView):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
-    permission_classes = [permissions.IsAdminUser | permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
 
 class ContactDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
-    permission_classes = [permissions.IsAdminUser | permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
 
 class GenreList(generics.ListCreateAPIView):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
 
 class GenreDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    permission_classes = [permissions.IsAdminUser | permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
