e5002ceef863fb73d1c24fed84e6cb88713b8ef3
nataliedutz <natalie.dutz@dci-student.org>
2024-04-19 10:22:17 +0200

    Added Swagger to semi-final

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,19 @@
-# api/urls.py
 from django.urls import path
 from .views import ContactListCreate, ContactDetail, GenreList, GenreDetail, APIRoot
+from rest_framework import routers, serializers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Our Book Store API",
+        default_version='v1',
+        description="API for genres and contacts for book store project",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('', APIRoot.as_view(), name='api-root'), 
@@ -8,5 +21,6 @@ urlpatterns = [
     path('contacts/<int:pk>/', ContactDetail.as_view(), name='contact-detail'),
     path('genres/', GenreList.as_view(), name='genre-list'),
     path('genres/<int:pk>/', GenreDetail.as_view(), name='genre-detail'),
-    
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
