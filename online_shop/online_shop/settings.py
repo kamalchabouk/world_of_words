a16f83b08468548145b8f522efb5ea5ae2fd3a2d
kamalchabouk <kamal.shabouk@gmail.com>
2024-04-22 10:51:45 +0200

    semi final update

--- online_shop/online_shop/settings.py
+++ online_shop/online_shop/settings.py
@@ -83,12 +83,7 @@ WSGI_APPLICATION = 'online_shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-""" DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-} """
+
 
 
 DATABASES = {
